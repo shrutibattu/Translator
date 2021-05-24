55abc0264661ab1e2abc9b0db072a8ff33e1e0f0
Shruti Battu <shrutibattu@MacBookPro-16inch.local>
2021-05-24 22:00:24 +0100

    added switch case function

--- translator.js
+++ translator.js
@@ -1,9 +1,38 @@
 export const translateEnglishToMorse = (word) => {
-  if (word === "a") {
-    return " .- ";
-  } else if (word === "b") {
-    return " -... ";
-  } else {
-    return "unavailable";
+  // if (word === "a") {
+  //   return ".-";
+  // } else if (word === "b") {
+  //   return "-...";
+  // } else if (word === "c") {
+  //   return "-.-.";
+  // } else {
+  //   return "unavailable";
+  // }
+  let alphabet = "unavailable";
+  switch (word) {
+    case "a":
+      alphabet = ".-";
+      break;
+    case "b":
+      alphabet = "-...";
+      break;
+    case "c":
+      alphabet = "-.-.";
+      break;
+    case "d":
+      alphabet = "-..";
+      break;
+    case "e":
+      alphabet = ".";
+      break;
+    case "f":
+      alphabet = "..-.";
+      break;
+    case "g":
+      alphabet = "--.";
+      break;
+    default:
+      alphabet = "unavailable";
   }
+  return alphabet;
 };
--- translator.test.js
+++ translator.test.js
@@ -12,3 +12,23 @@ it("should translate c to -.-.", () => {
   const result = translateEnglishToMorse("c");
   expect(result).toBe("-.-.");
 });
+it("should translate d to -..", () => {
+  const result = translateEnglishToMorse("d");
+  expect(result).toBe("-..");
+});
+it("should translate e to .", () => {
+  const result = translateEnglishToMorse("e");
+  expect(result).toBe(".");
+});
+it("should translate f to ..-.", () => {
+  const result = translateEnglishToMorse("f");
+  expect(result).toBe("..-.");
+});
+it("should translate g to --.", () => {
+  const result = translateEnglishToMorse("g");
+  expect(result).toBe("--.");
+});
+it("should translate default to unavailable", () => {
+  const result = translateEnglishToMorse("default");
+  expect(result).toBe("unavailable");
+});
